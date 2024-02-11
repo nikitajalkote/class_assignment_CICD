ace03c09eba2b37a7923b874f02d66b8c68f66c4
nikitajalkote <53075857+nikitajalkote@users.noreply.github.com>
2024-02-11 20:36:03 +0530

    Add files via upload
    added python script

---
+++ CICD_python_script.py
@@ -0,0 +1,36 @@
+import requests
+from datetime import datetime, timedelta
+
+def get_new_commits(username, repository, token, since):
+    url = f'https://api.github.com/repos/{username}/{repository}/commits'
+    headers = {'Authorization': f'token {token}'}
+    params = {'since': since}
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        commits = response.json()
+        return commits
+    else:
+        print(f"Failed to fetch commits. Status code: {response.status_code}")
+        return None
+
+def main():
+    username = 'username'
+    repository = 'git_assignment_HeroVired'
+    token = 'github token'
+    
+    # Set the datetime for the last check (e.g., one day ago)
+    since_datetime = (datetime.now() - timedelta(hours=1)).isoformat()
+
+    commits = get_new_commits(username, repository, token, since_datetime)
+
+    if commits:
+        print(f"New commits in {username}/{repository}:")
+        for commit in commits:
+            print(f"Commit: {commit['sha'][:7]} by {commit['commit']['author']['name']}: {commit['commit']['message']}")
+    else:
+        print("No new commits.")
+
+if __name__ == "__main__":
+    main()
