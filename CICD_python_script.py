fd385dbd191d329dd9b5f50f4d867cdf042f086a
nikitajalkote <53075857+nikitajalkote@users.noreply.github.com>
2024-02-17 20:14:36 +0530

    Update CICD_python_script.py
    PR5 token

--- CICD_python_script.py
+++ CICD_python_script.py
@@ -18,7 +18,7 @@ def get_new_commits(username, repository, token, since):
 def main():
     username = 'nikitajalkote'
     repository = 'class_assignment_CICD'
-    token = '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'
+    token = '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'
     
     # Set the datetime for the last check (e.g., one day ago)
     since_datetime = (datetime.now() - timedelta(hours=240)).isoformat()
